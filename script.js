4765715e7fa6e8048dadb920cff2f649724a808b
BassMasterTT <tothtibi22@gmail.com>
2021-01-11 23:02:29 +0800

    working updateDOM function

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
-const countdownEl = document.getElementById('countdown');
-const countdownElTitle = document.getElementById('countdown-title');
-const countdownBtn = document.getElementById('countdown-button');
-const timeElements = document.querySelectorAll('span');
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
 
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 
 const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date Input Minimum with Today's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
-
 // Populate Countdown / Complete UI
 function updateDOM() {
-    const now = new Date().getTime();
-    const distance = countdownValue - now;
-    console.log('distance', distance);
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  console.log("distance", distance);
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  console.log(days, hours, minutes, seconds);
 }
 
 // Take Values from Form Input
@@ -32,7 +40,7 @@ function updateCountdown(e) {
   console.log(countdownTitle, countdownDate);
   //   get number version of current Date, update DOM
   countdownValue = new Date(countdownDate).getTime();
-  console.log('countdown value', countdownValue);
+  console.log("countdown value", countdownValue);
   updateDOM();
 }
 
